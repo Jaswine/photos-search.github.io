443e833947230928b627564106781fbd240c1cbd
Vaseles <vaselesstolarov813@gmail.com>
2022-12-30 16:50:15 +0600

    03 - add pages & paginator in Unsplash

--- src/pages/Unsplash.js
+++ src/pages/Unsplash.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {MdImageSearch} from 'react-icons/md';
 import {AiTwotoneHeart} from   'react-icons/ai';
+import {TiArrowLeftOutline, TiArrowRightOutline} from 'react-icons/ti';
 
 
 function Unsplash() {
@@ -9,21 +10,41 @@ function Unsplash() {
     const [photoInfo, setPhotoInfo] = useState([]);
     const [image, setImage] = useState('');
     const [access_Key, setAccess_Key] = useState('')
+    const [page, setPage] = useState(1);
 
-    // useEffect(() => {
-    //     getPhotos()
-    // }, [])
+    useEffect(() => {
+        document.title = "Unsplash";
+    }, [])
 
-    const getPhotos = async (e) => {
-        e.preventDefault();
-        console.log(image);
+    console.log(page)
 
-       axios.get(`https://api.unsplash.com/search/photos?page=1&query=${image}&client_id=${access_Key}`)
-           .then(res => {
+    const getPhotos = () => {
+        if (page < 1) {
+            setPage(1)
+        }
+        console.log(page)
+        axios.get(`https://api.unsplash.com/search/photos?page=${page}&query=${image}&client_id=${access_Key}`)
+        .then(res => {
                 console.log(res.data);
                 setPhotoInfo(res.data);
                 setPhotos(res.data.results)
             })
+    }
+
+    const searchPhotos = async (e) => {
+        e.preventDefault();
+        getPhotos();
+    }
+
+    const left = () => {
+        if (page > 1) {
+            setPage(page-1)
+        }
+        getPhotos();
+    }
+    const right = () => {
+        setPage(page+1);
+        getPhotos();
     }
 
   return (
@@ -41,7 +62,13 @@ function Unsplash() {
                 onChange={(e) => setImage(e.target.value)}
                 placeholder='search image'
             />
-            <button className='btn' onClick={getPhotos}><MdImageSearch className='btn--icon'/></button>
+            <input 
+                type='number' 
+                className='search__input search__counter' 
+                value={page}
+                onChange={(e) => setPage(e.target.value)}
+             />
+            <button className='btn' onClick={searchPhotos}><MdImageSearch className='btn--icon'/></button>
         </form>
         <div className="page__filters"></div>
         <div className="page__photos">
@@ -49,8 +76,7 @@ function Unsplash() {
                 <div className="photos">
                     {photos.map(photo => 
                         <div className="photo" key={photo.id}>
-                            <img src={photo.urls.regular} alt={photo.description} />
-                            {console.log(photo)}
+                            <img src={photo.urls.regular} xalt={photo.description} />
                             <div className="photo__info">
                                 <p>{photo.description}</p>
                                 <a className='link photo__link'>{photo.user.name}</a>
@@ -66,6 +92,28 @@ function Unsplash() {
                 </div>
             )}
         </div>
+        
+        {photos.length != 0 ? (
+            <div className="paginator">
+            <div className="paginator__on__left">
+                <button 
+                    className="btn"
+                    onClick={left}
+                    >
+                        <TiArrowLeftOutline/>
+                </button>
+            </div>
+            <div className="paginator__on__right">
+            <button 
+                className="btn"
+                onClick={right}
+            >
+                <TiArrowRightOutline/>
+                </button>
+            </div>
+        </div>
+        ): (<></>)}
+
     </div>
   )
 }
